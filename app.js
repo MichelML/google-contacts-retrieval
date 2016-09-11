f745bc76242dd3d23f6b6af2a406d7449562e56f
MichelML <michmoreau.l@gmail.com>
2016-09-11 08:24:14 -0400

    refactor:transform functions to arrow format

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.set('view engine', 'pug');
 app.use(bodyParser.json());
 app.use(methodOverride('X-HTTP-Method-Override'));
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
     var locals = {
         title: 'This is my sample app',
         url: gapi.url
@@ -36,47 +36,47 @@ app.get('/', function(req, res) {
     res.render('index.jade', locals);
 });
 
-app.get('/oauth2callback', function(req, res) {
+app.get('/oauth2callback', (req, res) => {
     var code = req.query.code;
-    var emailsOfPersons;
+    var Emails;
     var pageRenderer = new EventEmitter();
-    gapi.client.getToken(code, function(err, tokens) {
+    gapi.client.getToken(code, (err, tokens) => {
         gapi.client.setCredentials(tokens);
         process.nextTick(
             () => {
                 gapi.google.plus('v1').people.get({
                     userId: 'me',
                     auth: gapi.client
-                }, function(err, response) {
+                }, (err, response) => {
                     // handle err and response
                     if (err) console.log(err);
                     else console.log(response);
                 });
-                simpleGet('https://people.googleapis.com/v1/people/me/connections?pageSize=500&requestMask.includeField=person.email_addresses%2Cperson.names&access_token=' + tokens.access_token, function(err, res) {
+                simpleGet('https://people.googleapis.com/v1/people/me/connections?pageSize=500&requestMask.includeField=person.email_addresses%2Cperson.names&access_token=' + tokens.access_token, (err, res) => {
                     if (err) throw err;
                     var persons = '';
-                    res.on('data', function(data) {
+                    res.on('data', (data) => {
                         persons += data;
                     });
-                    res.on('end', function() {
+                    res.on('end', () => {
                         persons = JSON.parse(persons);
-                        emailsOfPersons = persons.connections.filter(val => val.emailAddresses)
-                                                             .map(val=>val.emailAddresses[0].value);
+                        Emails = persons.connections.filter(person => person.emailAddresses)
+                            .map(person2 => person2.emailAddresses[0].value);
                         pageRenderer.emit('emailsReady');
                     });
-                    res.on('error', function() {
+                    res.on('error', () => {
                         console.log('There was an error. While streaming the data.')
                     });
                 });
             });
-        
+
         pageRenderer.on('emailsReady', () => {
-           var locals = {
+            var locals = {
                 title: 'My sample app',
                 url: gapi.url,
-                emails: emailsOfPersons
-            }; 
-           res.render('index.jade', locals);
+                emails: Emails
+            };
+            res.render('index.jade', locals);
         });
     });
 });
